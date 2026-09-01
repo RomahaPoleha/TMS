f7c0c3ced279c771a6642adc371cb2df6d5c4785
RomahaPoleha <romashkoo2023@list.ru>
2026-09-01 17:20:40 +1000

    добавлены все неоьбходимые столбцы в таблицу, прописана логика для свободно к продаже

--- inventory/views.py
+++ inventory/views.py
@@ -11,13 +11,13 @@ from inventory.models import Product
 
 def main_dashboard(request):
     products=Product.objects.annotate(
-        total_count = Count("unit", filter=Q(unit__status__in=["IN_STOCK", "READY", "IN_SERVICE"])),
+        total_count = Count("unit", filter=Q(unit__status__in=["IN_STOCK", "READY", "IN_SERVICE"])), # Фильтрация по нескольким значяениям
         ready_count = Count("unit", filter=Q(unit__status="READY")), # считаем Unit'ы, у которых статус READY
         service_count = Count("unit", filter=Q(unit__status="IN_SERVICE")), #
         reserved_count=Sum("reservation__quantity", filter=Q(reservation__is_fulfilled=False)), #  суммируем quantity из неисполненных резервов
         available_count=ExpressionWrapper(
             F('total_count') -
-            Coalesce(F('reserved_count'), Value(0)) -
+            Coalesce(F('reserved_count'), Value(0)) - # Coalesce заменяет None на значение по умолчанию в данном случае на 0
             Coalesce(F('service_count'), Value(0)),
             output_field=IntegerField()
         )
