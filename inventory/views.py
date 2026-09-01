6987016178b360c46cd733dda3c528b1154dfde5
RomahaPoleha <romashkoo2023@list.ru>
2026-09-01 17:27:23 +1000

    обернул reserved_count в Coalesce

--- inventory/views.py
+++ inventory/views.py
@@ -14,7 +14,7 @@ def main_dashboard(request):
         total_count = Count("unit", filter=Q(unit__status__in=["IN_STOCK", "READY", "IN_SERVICE"])), # Фильтрация по нескольким значяениям
         ready_count = Count("unit", filter=Q(unit__status="READY")), # считаем Unit'ы, у которых статус READY
         service_count = Count("unit", filter=Q(unit__status="IN_SERVICE")), #
-        reserved_count=Sum("reservation__quantity", filter=Q(reservation__is_fulfilled=False)), #  суммируем quantity из неисполненных резервов
+        reserved_count=Coalesce(Sum("reservation__quantity", filter=Q(reservation__is_fulfilled=False)),Value(0)), #  суммируем quantity из неисполненных резервов
         available_count=ExpressionWrapper(
             F('total_count') -
             Coalesce(F('reserved_count'), Value(0)) - # Coalesce заменяет None на значение по умолчанию в данном случае на 0
