6235884e231d286cdcc13757a0da991d3ee3c2a9
RomahaPoleha <romashkoo2023@list.ru>
2026-09-11 17:04:24 +1000

    добавлена функция прихода и функция создания номенклатуры, добавлен шаблон функции прихода

--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,12 @@
+from itertools import product
+from tkinter.font import names
+
 from django.db.models import F, ExpressionWrapper, IntegerField, Value
 from django.db.models.functions import Coalesce
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Count, Sum, Q, Subquery, OuterRef
 
-from inventory.models import Product, Reservation, ReservationItem, Unit
+from inventory.models import Product, Reservation, ReservationItem, Unit, EquipmentType
 
 
 # Вывод данных в главную таблицу
@@ -32,11 +35,63 @@ def main_dashboard(request):
         ).order_by('equipment_type__name', 'name')
     return render(request, "inventory/dashboard.html", {"products": products})
 
-
+# Вывод устройств в резерве
 def active_reservations(request):
     reservations = Reservation.objects.filter(is_fulfilled=False)
     return render(request, "inventory/reservations.html", {"reservations":reservations})
 
+# Вывод устройст в сервисе
 def active_service(request):
     units = Unit.objects.filter( status = "IN_SERVICE").order_by('service_received_at')
     return render(request, "inventory/service.html", {"units":units})
+
+# Функция прихода
+def add_units(request):
+    if request.method == "POST":
+        product_id = request.POST.get('product')
+        quantity = int(request.POST.get('quantity'))
+
+        product = Product.objects.get(id = product_id)
+
+        for _ in range(quantity):
+            Unit.objects.create(
+                product=product,
+                status = "IN_STOCK" # serial_number не указываем — он будет None
+            )
+        return redirect("dashboard")
+
+    products = Product.objects.all()
+    return render(request, "inventory/add_units.html", {"products": products})
+
+# Создание новой номенклатуры
+def add_product(request):
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        equipment_type_id = request.POST.get('equipment_type')
+        equipment_types = EquipmentType.objects.all()
+        error_message = None
+
+        if name and equipment_type_id:
+            # Проверяем дубликаты
+            if Product.objects.filter(name=name).exists():
+                error_message = "Товар с таким названием уже существует"
+            else:
+                equipment_type = EquipmentType.objects.get(id=equipment_type_id)
+                Product.objects.create(name=name, equipment_type=equipment_type)
+                return redirect('add_units')
+        else:
+            error_message = "Заполните все поля"
+
+
+
+        if error_message:
+            return render(request, "inventory/add_product.html", {
+                "equipment_types": equipment_types,
+                "error": error_message,})  # ← передаём ошибку в шаблон
+
+    return render(request, "inventory/add_product.html", {"equipment_types": equipment_types})
+
+
+
+
